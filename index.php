436504e3e798647d96041836bff185f11416f457
DiegoMartinez <diegofernandomartinezcalderon@gmail.com>
2023-06-07 17:26:29 -0500

    feat: :tada: diseño de formulario

---
+++ index.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <title>formulario</title>
+</head>
+
+<body>
+    <form>
+
+
+
+
+
+
+
+
+        <div class="container">
+            <br><br>
+            <div class="row">
+                <div class="col-lg-6">
+                    <hr>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-3">
+                    <input type="text" name="nombre" placeholder="Nombre">
+                </div>
+                <div class="col-lg-3">
+                   <h2>Campuslands</h2> 
+                </div>
+                <br /><br />
+            </div>
+            <div class="row">
+                <div class="col-lg-3">
+                    <input type="text" name="apellidos" placeholder="Apellidos">
+                </div>
+                <div class="col-lg-3">
+                    <input type="number" name="edad" placeholder="Edad"> <br /><br />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-3">
+                    <input type="text" name="direccion" placeholder="Direccion">
+                </div>
+                <div class="col-lg-3">
+                    <input type="text" name="email" placeholder="Email"><br /><br />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-6">
+                    <hr>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-3">
+                    <input type="time" name="horario" placeholder="Horario de entrada"> <label>Horario de entrada</label>
+
+                </div>
+                <div class="col-lg-1">
+                    <input type="button" value="✔">
+                </div>
+                <div class="col-lg-1">
+                    <input type="button" value="X"> <br /><br />
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-3">
+                    <input type="text" name="team" placeholder="Team">
+                </div>
+                <div class="col-lg-1">
+                    <input type="button" value="✎">
+                </div>
+                <div class="col-lg-1">
+                    <input type="button" value="search">
+                </div> <br /><br />
+            </div>
+            <div class="row">
+                <div class="col-lg-3">
+                    <input type="text" name="trainer" placeholder="Trainer">
+                </div>
+                <div class="col-lg-3">
+                    <input type="number" name="cedula" placeholder="cedula">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-6">
+                    <hr>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-6">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">Nombre</th>
+                                <th scope="col">Apellidos</th>
+                                <th scope="col">Direccion</th>
+                                <th scope="col">Edad</th>
+                                <th scope="col">Direccion</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">Horario entrada</th>
+                                <th scope="col">Team</th>
+                                <th scope="col">Trainer</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+    </form>
+
+</body>
+
+</html>
+
+
+
+
+<?php
+
+
+
+?>
